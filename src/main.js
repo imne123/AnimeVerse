6ba525c16d0c1a0772f57f19da6bba8f4ac3a1bf
Imane <imane.azehaf@student.ehb.be>
2026-05-11 19:37:43 +0200

    Zoekfunctie toegevoegd

--- src/main.js
+++ src/main.js
@@ -2,29 +2,57 @@ import './style.css';
 
 const app = document.querySelector('#app');
 
+let animeList = [];
+
+const displayAnime = (animes) => {
+    const animeContainer = document.querySelector('#anime-container');
+
+    animeContainer.innerHTML = '';
+
+    animes.forEach(anime => {
+        animeContainer.innerHTML += `
+            <div class="anime-card">
+                <img src="${anime.images.jpg.image_url}" alt="${anime.title}">
+                <h2>${anime.title}</h2>
+                <p>⭐ Score: ${anime.score}</p>
+            </div>
+        `;
+    });
+};
+
 const fetchAnime = async () => {
     try {
         const response = await fetch('https://api.jikan.moe/v4/top/anime');
 
         const data = await response.json();
 
-        const animeList = data.data;
+        animeList = data.data;
 
         app.innerHTML = `
             <h1>AnimeVerse</h1>
+
+            <input 
+                type="text" 
+                id="search"
+                placeholder="Zoek een anime..."
+            >
+
             <div id="anime-container"></div>
         `;
 
-        const animeContainer = document.querySelector('#anime-container');
+        displayAnime(animeList);
 
-        animeList.forEach(anime => {
-            animeContainer.innerHTML += `
-                <div class="anime-card">
-                    <img src="${anime.images.jpg.image_url}" alt="${anime.title}">
-                    <h2>${anime.title}</h2>
-                    <p>⭐ Score: ${anime.score}</p>
-                </div>
-            `;
+        const searchInput = document.querySelector('#search');
+
+        searchInput.addEventListener('input', () => {
+
+            const searchValue = searchInput.value.toLowerCase();
+
+            const filteredAnime = animeList.filter(anime =>
+                anime.title.toLowerCase().includes(searchValue)
+            );
+
+            displayAnime(filteredAnime);
         });
 
     } catch (error) {
